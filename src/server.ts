c4ceb196f18baa490e121404e50b33f7e0803a20
Júnior <valdecyborgesjr@hotmail.com>
2021-03-26 23:20:49 -0300

    Starting the project

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import express from 'express'; 
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'nlw 4 - nodejs'})
+})
+
+app.listen(3333, () => console.log('Server started on port: 3333!'))
