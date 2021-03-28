42be043bba0feb401c6839459e0aed118fb63e43
Júnior <valdecyborgesjr@hotmail.com>
2021-03-28 14:37:58 -0300

    Configuring eslint

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import 'reflect-metadata';
 import './database';
-import express from 'express'; 
+import express from 'express';
+
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'nlw 4 - nodejs'})
-})
+  res.json({ message: 'nlw 4 - nodejs' });
+});
 
-app.listen(3333, () => console.log('Server started on port: 3333!'))
+app.listen(3333, () => console.log('Server started on port: 3333!'));
